4eba6f9eea23b6a23ad8fc53d185d70ff3c2e639
oscar p <oscarperezm16@hotmail.com>
2021-09-10 20:40:50 -0500

    operacioes II

--- condicionales.py
+++ condicionales.py
@@ -17,7 +17,9 @@ else:
 
 total= compra-descuento;
 
+print();
 print('el valor total a pagar es de:','$',total);
+print();
 print('el descuento fue de:','$',descuento);    
 
 print('................................................')
@@ -33,7 +35,9 @@ else:
 
 totalc= total_compra-descuento;
 
+print();
 print('el valor total a pagar es de:','$',totalc);
+print();
 print('el descuento fue de:','$',descuento);    
    
 print('................................................')
@@ -48,7 +52,9 @@ else:
 
 pago= capital+interes;
 
+print();
 print('la cuota total es de:','$',pago);
+print();
 print('el interes es de:','$',interes);    
    
 print('................................................')
@@ -75,9 +81,13 @@ else:
     multa=0;
 gd= sumag-multa;
 
+print();
 print('el total de puntos recolectados en los  5 dias es:', sumad);
+print();
 print('el total de las ganancias de los 5 dias es de','$',sumag);
-print('la multa por contaminacion es de','$',multa);    
+print();
+print('la multa por contaminacion es de','$',multa);   
+print(); 
 print('el total de dinero recibido despues del proceso es','$',gd);
 
 print('................................................')
@@ -90,9 +100,11 @@ decremento= int(input("ingrese el decremento aunal del carro:"));
 incremento = (((precio_comun * incremento) / 100) * 3) / 2;
 decremento = ((precio_comun * decremento) / 100) * 3;
 
+print();
 print('la mitad del incremento del terreno al pasar 3 años es de:','$', incremento);
+print();
 print('el decremento del carro al pasar 3 años es de','$',decremento);
-
+print();
 if decremento<incremento:
     print('conviene comprar el carro');
 else:
@@ -114,7 +126,9 @@ else:
 
 total_des= totalpc-descuento_pc;
 
+print();
 print('el valor total a pagar por',numero_pc,'computadoras es de:','$',total_des);
+print();
 print('el descuento aplicado es de',"$", descuento_pc);    
     
 print('................................................')  
@@ -134,9 +148,13 @@ total_e = precio_e-descuento;
 iva_e=(total_e*iva)/100;
 total_final=total_e+iva_e;
 
+print();
 print('el total a pagar es','$',precio_e);
+print();
 print('el descuento aplicado es de','$',descuento);
+print();
 print('saldo total del iva','$',iva_e);
+print();
 print('el total del precio con iva incluido es de','$',total_final);    
 
 print('................................................') 
@@ -157,10 +175,15 @@ else:
 
 interess=credito*0.20;
 
+print();
 print('costo de las piesas','$',totalp);
+print();
 print('el monto a invertir es de:','$',invertir);
+print();
 print('el monto del prestamo r es de:','$',prestamo);
+print();
 print('el monto del credito es de:','$',credito);
+print();
 print('el interes es de:','$', interess);
     
 print('................................................')  
@@ -176,14 +199,24 @@ if n1>n2:
 if n1<n2:
     operacion=n1+n2;
 
+print();
 print('el resultado de la operacion es:', operacion);
     
 print('................................................')  
 
-
-
-
-
+#operaciones II
+n1=int(input('digite el primer numero:'));
+n2=int(input('digite el segundo numero:'));
+n3=int(input('digite el tercer numero:'));
+print();
+if n1>n2 and n1>n3:
+    print('el numero mayor es:',n1);
+if n2>n1 and n2>n3:
+    print('el numero mayor es:',n2);
+if n3>n1 and n3>n2:
+    print('el numero mayor es:',n3);    
+    
+print('................................................')  
 
 
 
